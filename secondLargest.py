b557b52c8ae32a50c0f2260c4268f0caa3927e8a
Rupam <rupam.mahato2697@gmail.com>
2026-01-13 19:40:02 +0530

    Added first file

---
+++ secondLargest.py
@@ -0,0 +1,21 @@
+user_input = input("Enter a list of numbers separated by spaces: ")
+numbers = list(map(int, user_input.split()))
+
+def find_second_largest(nums):
+    if len(nums) < 2:
+        return -1
+    largest = secondLargest = float('-inf')
+    for num in nums:
+        if num > largest:
+            secondLargest = largest
+            largest = num
+        elif num != largest and num > secondLargest:
+            secondLargest = num
+    
+    if secondLargest == float('-inf'):
+        return -1
+    
+    return secondLargest
+
+result = find_second_largest(numbers)
+print("The second largest number is:", result)
