63581d5916d831298290d0f73f16a73e014d9518
Rupam Mahato <rupam.mahato2697@gmail.com>
2026-02-02 19:43:51 +0530

    Update basic10.py

--- basic10.py
+++ basic10.py
@@ -6,6 +6,6 @@ r = 2
 ########### Write your code below ###############
 # Compute the GP Term
 ans = a * r ** (n-1)
-########### Write your code above ###############
 
+########### Write your code above ###############
 print(ans)
